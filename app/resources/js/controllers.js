18655a0936753330c3d65e7c047aaf6c72a91b49
Vineet Upadhyay <vinnieupadhyay@gmail.com>
2017-06-25 01:41:36 +0530

    added custom filter
    filter artist by country

--- app/resources/js/controllers.js
+++ app/resources/js/controllers.js
@@ -1,10 +1,56 @@
 myApp = angular.module('myApp', []);
 
+// Setup the filter
+myApp.filter('unique', function() {
+
+    return function (items, targetFields) {
+
+        var newItems = [];
+        var sameValue = function (item) {
+            return item[targetFields];
+        };
+
+        angular.forEach(items, function (item) {
+          var isDuplicate = false;
+
+          for (var i = 0; i < newItems.length; i++) {
+            if (angular.equals(sameValue(newItems[i]), sameValue(item))) {
+              isDuplicate = true;
+              break;
+            }
+          }
+          if (!isDuplicate) {
+            newItems.push(item);
+          }
+
+        });
+        items = newItems;
+      return items;
+    };
+
+
+    //Code didn't work due to error: property 'length' of undefined
+    // return function(items, fields) {
+    //     var tempObj = {},
+    //         result = [];
+    //
+    //     for(i=0; i< items.length; i++) {
+    //         tempObj[items[i][fields]] = items[i];
+    //     }
+    //
+    //     for(key in tempObj) {
+    //       final.push(tempObj[key]);
+    //     }
+    //
+    //     return result;
+    // };
+
+});
+
 myApp.controller('myController', function($scope, $http) {
 
     $http.get('resources/js/data.json').then(function(response) {
         $scope.artists = response.data;
-        console.log($scope.artists);
     });
 
 });
